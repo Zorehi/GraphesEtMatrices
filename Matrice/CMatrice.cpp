f324fb4037ce10fc61814befe4ff4fcfa6bdf4b6
Zorehi <jeremy284@hotmail.com>
2022-04-27 10:13:33 +0200

    Update

--- Matrice/CMatrice.cpp
+++ Matrice/CMatrice.cpp
@@ -1,25 +1,4 @@
+//#include "CMatrice.h"
 
-#include "CMatrice.h"
 
-template <typename ELEMENT>
-CMatrice<ELEMENT>::CMatrice()
-{
-}
 
-template <typename ELEMENT>
-CMatrice<ELEMENT>::CMatrice(unsigned int uiParamNbLignes, unsigned int uiParamNbColonnes, char* pcParamTypeMatrice)
-{
-	
-
-}
-
-template <typename ELEMENT>
-CMatrice<ELEMENT>::CMatrice(CMatrice& MATParam)
-{
-
-}
-
-template <typename ELEMENT>
-CMatrice<ELEMENT>::~CMatrice()
-{
-}
--- Matrice/CMatrice.h
+++ Matrice/CMatrice.h
@@ -2,22 +2,117 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "CListe.h"
+#include "CProxy_row.h"
+
+#define EXCOutOfMemory 1
 
 template <typename ELEMENT>
 class CMatrice {
-	
-	private :	
-		unsigned int uiMABNbLignes;
-		unsigned int uiMABNbColonnes;
-		CListe<ELEMENT>* pLISMATListe;
+	using MATPROProxy = CProxy_row<CListe<ELEMENT>>;
 
-	public :
-		CMatrice();
-		CMatrice(unsigned int uiParamNbLignes, unsigned int uiParamNbColonnes, char* pcParamTypeMatrice);
-		CMatrice(CMatrice& MATParam);
-		~CMatrice();
+private:
+	char* pcTypeMatrice;
+	unsigned int uiMABNbLignes;
+	unsigned int uiMABNbColonnes;
+	CListe<ELEMENT>* pLISMATListe;
 
 
+public:
+	CMatrice();
+	CMatrice(unsigned int uiParamNbLignes, unsigned int uiParamNbColonnes);
+	~CMatrice();
 
+
+	void MABModifierTypeMatrice(const char* pcParamTypeMatrice);
+	void MABModifierNbLignes(unsigned int uiParamNbLignes);
+	void MABModifierNbColonnes(unsigned int uiParamNbColonnes);
+
+	char* MABLireTypeMatrice();
+	unsigned int MABLireNbLignes();
+	unsigned int MABLireNbColonnes();
+	//ELEMENT& operator[](unsigned int uiLignes);
+	MATPROProxy operator[](unsigned int uiLignes) {
+		return MATPROProxy(*pLISMATListe, uiLignes, uiMABNbColonnes);
+	}
 };
+
+template <typename ELEMENT>
+CMatrice<ELEMENT>::CMatrice()
+{
+	pcTypeMatrice = NULL;
+	uiMABNbLignes = 0;
+	uiMABNbColonnes = 0;
+	pLISMATListe = new CListe<ELEMENT>();
+}
+
+template <typename ELEMENT>
+CMatrice<ELEMENT>::CMatrice(unsigned int uiParamNbLignes, unsigned int uiParamNbColonnes)
+{
+	pcTypeMatrice = NULL;
+	uiMABNbLignes = uiParamNbLignes;
+	uiMABNbColonnes = uiParamNbColonnes;
+	unsigned int uiTailleListe = uiParamNbLignes * uiParamNbColonnes;
+	pLISMATListe = new CListe<ELEMENT>(uiTailleListe);
+}
+
+template <typename ELEMENT>
+CMatrice<ELEMENT>::~CMatrice()
+{
+	if (pcTypeMatrice) {
+		free(pcTypeMatrice);
+	}
+}
+
+template <typename ELEMENT>
+void CMatrice<ELEMENT>::MABModifierTypeMatrice(const char* pcParamTypeMatrice)
+{
+	if (pcTypeMatrice) {
+		free(pcTypeMatrice);
+	}
+
+	pcTypeMatrice = (char*)malloc(sizeof(char) * strlen(pcParamTypeMatrice) + 1);
+	if (!pcTypeMatrice) {
+		//erreur memoire
+	}
+	else {
+		strcpy_s(pcTypeMatrice, strlen(pcParamTypeMatrice) + 1, pcParamTypeMatrice);
+	}
+}
+
+template <typename ELEMENT>
+void CMatrice<ELEMENT>::MABModifierNbLignes(unsigned int uiParamNbLignes)
+{
+	uiMABNbLignes = uiParamNbLignes;
+}
+
+template <typename ELEMENT>
+void CMatrice<ELEMENT>::MABModifierNbColonnes(unsigned int uiParamNbColonnes)
+{
+	uiMABNbColonnes = uiParamNbColonnes;
+}
+
+template <typename ELEMENT>
+char* CMatrice<ELEMENT>::MABLireTypeMatrice()
+{
+	return pcTypeMatrice;
+}
+
+template <typename ELEMENT>
+unsigned int CMatrice<ELEMENT>::MABLireNbLignes()
+{
+	return uiMABNbLignes;
+}
+
+template <typename ELEMENT>
+unsigned int CMatrice<ELEMENT>::MABLireNbColonnes()
+{
+	return uiMABNbColonnes;
+}
+
+
+//template <typename ELEMENT>
+//ELEMENT& CMatrice<ELEMENT>::operator[](unsigned int uiLignes) {
+//	return (*pLISMATListe)[uiLignes];
+//}
