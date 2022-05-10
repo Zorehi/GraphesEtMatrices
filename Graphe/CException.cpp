163f24532b9e54e132674fc7b5a259075e70085a
Zorehi <jeremy284@hotmail.com>
2022-05-10 16:20:14 +0200

    Ajout de CException dans le projet Graphe

---
+++ Graphe/CException.cpp
@@ -0,0 +1,72 @@
+/**
+ * @file CException.cpp
+ * @author LJ BV JM
+ * @brief Contient les definitions des methodes de la classe CException
+ * CException permet de gerer les erreurs
+ * 
+ * @version 0.1
+ * @date 2022-05-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "CException.h"
+
+CException::CException()
+{
+	iEXCVal = 0;
+	pcEXCMsg = NULL;
+}
+
+CException::CException(int iNewVal, const char* pcNewMsg)
+{
+	iEXCVal = iNewVal;
+	pcEXCMsg = (char*)malloc((strlen(pcNewMsg) + 1) * sizeof(char));
+	if (pcEXCMsg != NULL) {
+		strcpy_s(pcEXCMsg, strlen(pcNewMsg) + 1, pcNewMsg);
+	}
+}
+
+CException::~CException()
+{
+	if (pcEXCMsg) {
+		free(pcEXCMsg);
+	}
+}
+
+
+int CException::EXCLireVal()
+{
+	return iEXCVal;
+}
+
+
+const char* CException::EXCLireMsg()
+{
+	return pcEXCMsg;
+}
+
+
+void CException::EXCModifierVal(int iNewVal)
+{
+	iEXCVal = iNewVal;
+}
+
+
+void CException::EXCModifierMsg(const char* pcNewMsg)
+{
+	if (pcEXCMsg) {
+		free(pcEXCMsg);
+	}
+
+	if (pcNewMsg) {
+		pcEXCMsg = (char*)malloc((strlen(pcNewMsg) + 1) * sizeof(char));
+		if (pcEXCMsg != NULL) {
+			strcpy_s(pcEXCMsg, strlen(pcNewMsg) + 1, pcNewMsg);
+		}
+	}
+	else {
+		pcEXCMsg = NULL;
+	}
+}
--- Graphe/Graphe.cpp
+++ Graphe/Graphe.cpp
@@ -1,9 +1,21 @@
-// Graphe.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+/**
+ * @file Graphe.cpp
+ * @author LJ BV JM
+ * @brief Contient la fonction main du programme
+ *
+ *
+ * @version 0.1
+ * @date 2022-05-10
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
 #include <iostream>
 
-int main()
+#include "CException.h"
+
+int main(int iArgc, char* ppcArgv[])
 {
     
 }
