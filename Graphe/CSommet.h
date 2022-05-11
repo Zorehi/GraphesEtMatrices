136b5d6ddff40e380bfbb5bc49d33ea7373203f6
Mathieu Jehanno <mathieujehannodu26@gmail.com>
2022-05-11 17:26:55 +0200

    correction pblm CSommet

--- Graphe/CSommet.h
+++ Graphe/CSommet.h
@@ -8,8 +8,8 @@ class CSommet
 {
 private:
 	int iSOMNumero;
-	CListe<CArc*> ppSOMArrivant;
-	CListe<CArc*> ppSOMPartant;
+	CListe<CArc*>* pLISSOMArrivant;
+	CListe<CArc*>* pLISSOMPartant;
 public:
 	CSommet();
 	CSommet(int iNumero);
@@ -18,8 +18,8 @@ public:
 
 	int SOMLireNumero();
 	void SOMModifierNumero(int iNumero);
-	CListe<CArc*> SOMLireArrivant();
-	CListe<CArc*> SOMLirePartant();
+	CListe<CArc*>& SOMLireArrivant();
+	CListe<CArc*>& SOMLirePartant();
 	void SOMAjouterArrivant(CArc* pArcArrivant);
 	void SOMAjouterPartant(CArc* pArcPartant);
 	void SOMSupprimerArrivant(CArc* pArcArrivant);
