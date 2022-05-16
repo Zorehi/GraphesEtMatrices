588d463f36dcc1fb81d590151ced529769173d6b
Zorehi <jeremy284@hotmail.com>
2022-05-16 10:15:44 +0200

    Destructeur fonctionnel

--- Graphe/CGraphe.h
+++ Graphe/CGraphe.h
@@ -1,3 +1,17 @@
+/**
+ * @file CGraphe.h
+ * @author LJ BV JM
+ * @brief Contient la declaration de la classe CGraphe
+ * CGraphe permet de creer et gerer un graphe contenant
+ * des sommets relier par des arcs
+ *
+ * @version 0.1
+ * @date 2022-05-02
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #pragma once
 
 #include <iostream>
@@ -13,16 +27,50 @@ private:
 	CListe<CSommet*> LISGRASommet;
 
 public:
+	/**
+	 * @brief Constructeur par defaut : Creer un objet Graphe
+	 *
+	 */
 	CGraphe();
-	CGraphe(int iNbSommet, int* piSommet, int iNbArc, int** ppiArc);
+
+	/**
+	 * @brief Constructeur de recopie : Creer un duplicata d'un Graphe deja cree
+	 *
+	 * @param GRAParam Graphe a dupliquer
+	 */
 	CGraphe(const CGraphe& GRAParam);
+
+	/**
+	 * @brief Destructeur : Libere la memoire alloue pour l'objet Graphe
+	 *
+	 */
 	~CGraphe();
 
+	/**
+	 * @brief Getter pour la liste de sommet du Graphe
+	 * 
+	 * @return const CListe<CSommet*>& objet CListe contenant les pointeur sur les objets sommets
+	 */
 	const CListe<CSommet*>& GRALireSommet()const;
+
+	/**
+	 * @brief Methode pour ajouter un sommet au graphe
+	 * 
+	 * @param pSommet Pointeur sur l'objet sommet à ajouter
+	 */
 	void GRAAjouterSommet(CSommet* pSommet);
 
+	/**
+	 * @brief Methode pour afficher le graphe sur le shell
+	 * 
+	 */
 	void GRAAfficherGraphe()const;
-	void GRAAfficherGrapheGraphique()const;
 
+	/**
+	 * @brief Surcharge de l'operateur d'assignation
+	 * 
+	 * @param GRAParam Graphe a copier
+	 * @return CGraphe& Graphe retourne
+	 */
 	CGraphe& operator=(const CGraphe& GRAParam);
 };
