36f9f4d283346e6517918b0f881a0f7088556ad1
Valentin | Electricien <valent200120012001@gmail.com>
2022-05-16 15:03:26 +0200

    Ajout de suppression d'un sommet Ajout de modification du numero d'un sommet Ajout de la possibilite d'ajouter un arc directement dans Cgraphe Ajout de la possibilité d'ajouter un sommet directement dans CGraphe Ajout d'un type de Graphe permettant de gérer les graphe non orientés

--- Graphe/CGraphe.h
+++ Graphe/CGraphe.h
@@ -11,15 +11,28 @@ class CGraphe
 {
 private:
 	CListe<CSommet*> LISGRASommet;
+	unsigned int uiType;
 
 public:
 	CGraphe();
+	CGraphe(unsigned int uiType);
 	CGraphe(int iNbSommet, int* piSommet, int iNbArc, int** ppiArc);
 	CGraphe(const CGraphe& GRAParam);
 	~CGraphe();
 
+
+
 	const CListe<CSommet*>& GRALireSommet()const;
+	const unsigned int GRALireType()const;
+
 	void GRAAjouterSommet(CSommet* pSommet);
+	void GRAAjouterArc(int iDebut, int iDestination);
+
+	void GRAModifierSommet(CSommet* pSommet, int iNewNum);
+	void GRAModifierSommet(int iOldNum, int iNewNum);
+
+	void GRASupprimerSommet(CSommet* pSommet);
+	void GRASupprimerSommet(int iNumSommet);
 
 	void GRAAfficherGraphe()const;
 	void GRAAfficherGrapheGraphique()const;
