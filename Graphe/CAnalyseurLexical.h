5c301d6cb98dfe52884e8dafb8be54af61cf62de
Valentin | Electricien <valent200120012001@gmail.com>
2022-05-20 15:47:58 +0200

    Commentaire doxygen pour les analyseurs

--- Graphe/CAnalyseurLexical.h
+++ Graphe/CAnalyseurLexical.h
@@ -25,26 +25,26 @@ class CAnalyseurLexical
 {
 	public:
 		/**
-		 * @brief 
+		 * @brief Ouvre et analyse un fichier texte pour en extraire les différents mots séparés par " ,=\r\n"
 		 * 
 		 * @param pcNomFichier Chaine de caractere vers le chemin du fichier
-		 * @return CListe<char*> 
+		 * @return CListe<char*> liste des mots contenus dans le fichier texte
 		 */
 		static CListe<char*> ANLAnalyserFichier(const char* pcNomFichier);
 
 		/**
-		 * @brief 
+		 * @brief Recupere la prochaine ligne du fichier
 		 * 
 		 * @param pFILFichier Pointeur vers l'instance du fichier a lire
-		 * @return char* 
+		 * @return char* Chaine de charactere contenant la ligne
 		 */
 		static char* ANLLireProchaineLigne(FILE* pFILFichier);
 
 		/**
-		 * @brief 
+		 * @brief Analyse une ligne de charactere pour en extraire les mots séparés par " ,=\r\n" et les ajoutes à la liste des mots placé en parametre
 		 * 
-		 * @param pcLigne 
-		 * @param LISCorrespondance 
+		 * @param pcLigne Ligne a analyser
+		 * @param LISCorrespondance Liste dans laquelle ajouter les mots trouvé sur la ligne
 		 */
 		static void ANLExtraireInfoLigne(const char* pcLigne, CListe<char*>& LISCorrespondance);
 
--- Graphe/CAnalyseurSyntaxique.h
+++ Graphe/CAnalyseurSyntaxique.h
@@ -22,7 +22,7 @@ class CAnalyseurSyntaxique
 {
 	public:
 		/**
-		 * @brief 
+		 * @brief Construit un Graphe à partir du chemin d'un fichier texte définissant un graphe
 		 * 
 		 * @param pcCheminFichier Chaine de caractere vers le chemin du fichier
 		 * @return CGraphe Graphe cree par la suite de la lecture du fichier
@@ -30,36 +30,36 @@ class CAnalyseurSyntaxique
 		static CGraphe ANSAnalyserFichier(const char* pcCheminFichier);
 
 		/**
-		 * @brief 
+		 * @brief Cherche le nombre de sommet en analysant la liste des mots
 		 * 
-		 * @param LISListeMot 
-		 * @return int 
+		 * @param LISListeMot La liste des mots retournees par l'analyseur lexical
+		 * @return int Le nombre de sommet
 		 */
 		static int ANSExtraireNbSommet(CListe<char*>& LISListeMot);
 		
 		/**
-		 * @brief 
+		 * @brief Cherche le nombre d'arc en analysant la liste des mots
 		 * 
-		 * @param LISListeMot 
-		 * @return int 
+		 * @param LISListeMot La liste des mots retournees par l'analyseur lexical
+		 * @return int Le nombre d'arcs
 		 */
 		static int ANSExtraireNbArc(CListe<char*>& LISListeMot);
 
 		/**
-		 * @brief 
+		 * @brief Cherche les numero de chaque sommet
 		 * 
-		 * @param LISListeMot 
-		 * @param iNbSommet 
-		 * @return CListe<int> 
+		 * @param LISListeMot La liste des mots retournees par l'analyseur lexical
+		 * @param iNbSommet Le nombre de sommet dans le graphe
+		 * @return CListe<int> Une liste des numéros des différents sommets
 		 */
 		static CListe<int> ANSExtraireSommets(CListe<char*>& LISListeMot, int iNbSommet);
 
 		/**
-		 * @brief 
+		 * @brief Recupere les informations sur les differents arcs
 		 * 
-		 * @param LISListeMot 
-		 * @param iNbArc 
-		 * @return CListe<int> 
+		 * @param LISListeMot La liste des mots retournees par l'analyseur lexical
+		 * @param iNbArc Le nombre d'arc
+		 * @return CListe<int> Une liste des provenance et destination de chaque arc [provenance, destination, provenance, destination, ...]
 		 */
 		static CListe<int> ANSExtraireArcs(CListe<char*>& LISListeMot, int iNbArc);
 };
